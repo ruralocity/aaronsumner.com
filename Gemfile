876cbb0b96f2fe306ffd85e91aaf3b8f261a7a52
Aaron Sumner <asumner@mac.com>
2021-11-14 13:58:30 -0800

    Upgrade to Middleman 4.3.11
    * Update dependencies
    * Fix broken use of `with_layout` method in configuration
    * Fix calls to rack-rewrite middleware in configs
    * Re-apply implicit dependency on nokogiri
    * Use native Ruby to show date in page layout
    * Remove outdated documentation comments in configs

--- Gemfile
+++ Gemfile
@@ -2,15 +2,16 @@
 # the following line to use "https"
 source 'https://rubygems.org'
 
-gem "middleman", "3.4.1"
+gem "middleman", "~> 4.3.11"
 
 # Live-reloading plugin
-gem "middleman-livereload", "~> 3.1.0"
+gem "middleman-livereload", "~> 3.4.7"
 
 # Blogging
-gem "middleman-blog", "~> 3.5.1"
+gem "middleman-blog", "~> 4.0.3"
 gem "builder"
+gem "nokogiri"
 
-gem "rack-rewrite", "~> 1.4.1"
+gem "rack-rewrite"
 
-gem "eventmachine", "~> 1.0.9"
+gem "eventmachine"
--- config.rb
+++ config.rb
@@ -1,65 +1,14 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-with_layout :page do
-  page "/pages/*"
-end
-
+page "/pages/*", :layout => :page
 page "/atom.xml", :layout => false
 
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
 set :css_dir, 'css'
-
 set :js_dir, 'js'
-
 set :images_dir, 'images'
 
 # Handle redirects from old post format in development
 require 'rack/rewrite'
-use Rack::Rewrite do
+use ::Rack::Rewrite do
   r301 '/projects.html', '/pages/projects.html'
   r301 '/archives.html', '/pages/archives.html'
   r301 '/colophon.html', '/pages/colophon.html'
